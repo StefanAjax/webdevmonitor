1446c9723642c61d900772fbfe96ca29e6af5030
StefanAjax <pianoajax@gmail.com>
2026-01-12 15:27:31 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-const { createServer } = require('./server');
-const { startScheduler } = require('./scheduler');
+const { createServer } = require("./server");
+const { startScheduler } = require("./scheduler");
 
 const PORT = process.env.PORT || 3000;
 
 // Catch unhandled errors to prevent crashes
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
 });
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
 
 const app = createServer();
 
 app.listen(PORT, () => {
   console.log(`Web Monitor running at http://localhost:${PORT}`);
-  console.log('Open the URL in your browser to manage websites');
+  console.log("Open the URL in your browser to manage websites");
   startScheduler();
 });
+
+// test
